58574690f50626858e53b2c0c79a79464d212014
BorisVV <bcarlof@gmail.com>
2018-02-15 21:37:50 -0600

    fixing problem with ORM relationships

--- website_config.py
+++ website_config.py
@@ -4,7 +4,7 @@ _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True # TODO: Change this to False.
 
-SECRET_KEY = 'testkey'
+# SECRET_KEY = 'testkey'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///sports.db'
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'sport-team.db')
 
