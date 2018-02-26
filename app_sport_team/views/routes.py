af31436d93e2dccf3faef73a6f088e0347d7554a
BorisVV <bcarlof@gmail.com>
2018-02-26 15:07:10 -0600

    Added comments and try except

--- app_sport_team/views/routes.py
+++ app_sport_team/views/routes.py
@@ -1,86 +1,121 @@
 
 from flask import \
-        render_template, redirect, url_for, g, abort, request, flash, \
-        Response
+        render_template, redirect, url_for, abort, request, flash
 
 from app_sport_team import app
 from app_sport_team.tables_setUp import \
             db_session, MerchandiseItems, SalesOfItems, DatesOfGames
 
 # Query all the merchandise items.
-def items_query():
+def items_query(): # TODO:  find a better way to do it?
     return MerchandiseItems.query.all()
 
 # Query all the game's dates.
-def dates_query():
+def dates_query(): # TODO:  find a better way to do it?
     return DatesOfGames.query.all()
 
-
+# For example purpose, this is the first pages displayed.
 @app.route('/')
-def base():
+def base(): # TODO: remove in future.
     return render_template('base.html')
 
+# This page will display the table for the items and options for others pages.
 @app.route('/home/')
 def home():
     items = items_query()
     return render_template('home.html', items=items)
 
-@app.route('/home/add_items/', methods=['GET', 'POST'])
+# :flash: In the base.html there is a block inside the body block that exexutes
+# the messsages. uses the syntax (var = get_flashed_messages()) and
+# will display messages for users to read and correct inputs, etc.
+
+# User can enter new items, form is displayed.
+@app.route('/add_items/', methods=['GET', 'POST'])
 def addItems():
     items = items_query()
     if request.method == 'POST':
-        new_name = request.form['name'].capitalize()
+
         if 'cancel' in request.form:
             return redirect(url_for('home'))
 
-        if new_name == "":
-            flash(u'Oops! It cannot be blank')
-            return redirect(url_for('addItems'))
+        else:
+            # Save button was clicked.
+            # str.capitalize()
+            new_name = request.form['name'].capitalize()
 
-        name_in_table = False
-        for name in items:
-            if new_name == name.name:
-                flash(u'Name %r already exist. <br>Try a different one' % new_name)
-                name_in_table = True
+            if new_name == "":  # validate input.
+                flash(u'Oops! It cannot be blank')
                 return redirect(url_for('addItems'))
 
-        if name_in_table == False:
+            for name in items:
+                if  name.name == new_name: # if name already in table.
+                    flash(u'Name %r already exist. <br>Try a different one' % new_name)
+                    return redirect(url_for('addItems'))
+
+            # The new name will be add if passed the above stataments.
             db_session.add(MerchandiseItems(name=new_name))
             db_session.commit()
             flash(u'Success! %r was added.' % new_name + u'<br>Do you want ' \
                     'to add another one? "Cancel to exit"')
-            return redirect(url_for('addItems'))
+            return redirect(url_for('addItems')) # option to add more.
 
     return render_template('addItems.html', items=items)
 
+# TODO: add option to add the id #/<int:id>/ or item being modified on the url.
 
-@app.route('/editItems/', methods=['GET', 'POST']) #/<int:id>/
+@app.route('/edit_items/', methods=['GET', 'POST'])
 def editItems():
     items = items_query()
     if request.method == 'POST':
-        rm_id = MerchandiseItems.query.get(request.form['_item'])
-        new_name = request.form['new_name']
+        # Query to get the item's name with the selected id from the form.
+        items_row = MerchandiseItems.query.get(request.form['_item_id'])
+        # item_id = request.form['_item_id'] # gets the id number only.
+
         if 'delete' in request.form:
-            db_session.delete(rm_id)
+            # name = items_row.name
+            db_session.delete(items_row)
             db_session.commit()
-            flash(u'%r was deleted' % rm_id)
-            return redirect(url_for('home'))
-        if 'update' in request.form:
-            if new_name != "":
-                rm_id.name = new_name
+            flash(u'%r was deleted <br> Do you want to delete/update more.\
+                    <br>"Cancel" to quit' % items_row.name)
+            return render_template('editItems.html', items=items)
+
+        elif 'update' in request.form:
+            new_name = request.form['new_name']
+            if new_name == "":
+                flash(u'Text box cannot be blank, name required! <br> \
+                        Enter new  name for the selected name! --> ' + items_row.name)
+                return render_template('editItems.html', items=items, item_id=items_row.id)
+
+            try:
+                old_name = items_row.name # Stored to display old name.
+                items_row.name = new_name.capitalize() # Change the name.
                 db_session.commit()
-                return redirect(url_for('home'))
-        else:
+                # Display message.
+                flash(u'%r was updated succesfully! with --> %r' \
+                        % (old_name, new_name))
+                flash(u'delete/update another? "Cancel" to quit')
+                # Render the template again with the updated item selected.
+                return render_template('editItems.html', items=items, item_id=items_row.id)
+            except:
+                db_session.rollback() # If not Exception will raise.
+                flash('%r is already in file, try a different name' % new_name)
+                # Re-render the template with the users inputs.
+                return render_template('editItems.html', items=items, item_id=items_row.id, new_name=new_name)
+
+        else: # If cancel button is clicked.
             return redirect(url_for('home'))
+
     return render_template('editItems.html', items=items)
 
-@app.route('/addDates/', methods=['GET', 'POST'])
+@app.route('/add_dates/', methods=['GET', 'POST'])
 def addDates():
+    # Enter new dates for games.
     _dates = dates_query()
+
+    form = dict(game_date=_dates.game_date, )
 
     if request.method == 'POST':
         date_game = request.form['date_game']
-
         city = request.form['city']
         state = request.form['state']
 
